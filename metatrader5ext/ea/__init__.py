9924afc583b6279ae1bc9a52b527e5f01cdda977
tradershubspace <hello.tradershub@gmail.com>
2025-02-24 04:50:39 -0800

    Comment out MetaTrader5Streamer import and update __all__ to reflect changes

--- metatrader5ext/ea/__init__.py
+++ metatrader5ext/ea/__init__.py
@@ -2,23 +2,23 @@ from metatrader5ext.ea.client import EAClient
 from metatrader5ext.ea.connection import Connection
 from metatrader5ext.ea.errors import ERROR_DICT
 
-try:
-    from metatrader5ext.ea.data_stream import MetaTrader5Streamer
+# try:
+#     from metatrader5ext.ea.data_stream import MetaTrader5Streamer
 
-    # TODO: Switch Streaming core to MQTT:
-    # https://github.com/eclipse-paho/paho.mqtt.python/
-    # https://en.wikipedia.org/wiki/MQTT
-except ImportError as e:
-    raise ImportError(
-        "Failed to import MetaTrader5Streamer. Ensure that data_stream file exists in the metatrader5ext directory and is properly compiled."
-    ) from e
+#     # TODO: Switch Streaming core to MQTT:
+#     # https://github.com/eclipse-paho/paho.mqtt.python/
+#     # https://en.wikipedia.org/wiki/MQTT
+# except ImportError as e:
+#     raise ImportError(
+#         "Failed to import MetaTrader5Streamer. Ensure that data_stream file exists in the metatrader5ext directory and is properly compiled."
+#     ) from e
 
 
 __all__ = [
     "EAClient",
     "Connection",
     "ERROR_DICT",
-    "MetaTrader5Streamer",
+    # "MetaTrader5Streamer",
 ]
 
 """
