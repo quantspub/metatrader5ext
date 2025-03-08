d9bf92fffe5a91891268eeba94d9c0417d0730e7
tradershubspace <hello.tradershub@gmail.com>
2025-03-08 06:17:31 +0100

    Add TimeFrameAggregator class for custom timeframe aggregation using polars

--- metatrader5ext/timeframe_agg.py
+++ metatrader5ext/timeframe_agg.py
@@ -1,8 +1,8 @@
 import polars as pl
 
-class Aggregator:
+class TimeFrameAggregator:
     """ 
-    Aggregator class for creating custom timeframes using polars for internal aggregation.
+    The class for creating custom timeframes using polars for internal aggregation.
     
     This class provides methods to aggregate data into custom timeframes, leveraging the 
     performance and flexibility of the polars library.
