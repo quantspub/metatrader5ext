9f368768a3277516165ddfbaeeaf851433c508ea
tradershubspace <hello.tradershub@gmail.com>
2025-03-08 06:40:01 +0100

    Refactor Mode enum to simplify client modes and clarify IPC usage on Linux

--- metatrader5ext/common.py
+++ metatrader5ext/common.py
@@ -14,19 +14,17 @@ DOUBLE_INFINITY = math.inf
 class Mode(Enum):
     """Mode type.
     
-    Includes 5 client modes: IPC, RPYC, EA, EA_IPC, and EA_RPYC.
+    Includes 3 client modes: IPC, EA, and EA_IPC.
 
     IPC: Mode for MetaTrader IPC communication on Windows.
-    RPYC: Mode for MetaTrader RPYC communication on Linux.
     EA: Mode for EA communication using sockets.
-    EA_IPC: Mode for combining EA and IPC communication.
-    EA_RPYC: Mode for combining EA and RPYC communication.
+    EA_IPC: Mode for combining EA and IPC communication (EA for streaming and IPC for request-reply).
+    
+    Since RPYC is used for IPC communication in Linux, it will be used for IPC mode in Linux (MetaTrader RPYC communication on Linux).
     """
     IPC = "IPC"
-    RPYC = "RPYC"
     EA = "EA"
     EA_IPC = "EA_IPC"
-    EA_RPYC = "EA_RPYC"
 
     def to_str(self) -> str:
         """Returns the string representation of the enum value."""
